021430ac4d6cc4fb48675d633a8aa681c943a992
Fran <franciscodumont1@gmail.com>
2019-11-06 18:33:49 -0300

    Metodo regresivo

--- newton_gregory.py
+++ newton_gregory.py
@@ -18,9 +18,30 @@ def newton_gregory(*puntos: Tuple[int, int]):
     print_tabla_diferencias_divididas(puntos, tabla)
 
     # Armo el polinomio
-    sumatoria = tabla[0][0]
+    sumatoria = tabla[0][1]
     for i in range(1, cantidad_puntos):
         sumatoria += (proterm(i, x, puntos) * tabla[0][i])
+
+    return sumatoria
+
+
+def newton_gregory_regresivo(*puntos: Tuple[int, int]):
+    cantidad_puntos = len(puntos)
+
+    # Creo la tabla llena de 0s
+    tabla = [[0 for i in range(cantidad_puntos)] for j in range(cantidad_puntos)]
+    # Cargo los f(x) de la tabla
+    for indice in range(cantidad_puntos):
+        tabla[indice][0] = puntos[indice][1]
+    # Genero la tabla de diferencias divididas
+    tabla = tabla_diferencias_divididas_regresiva(puntos, tabla, cantidad_puntos);
+
+    print_tabla_diferencias_divididas(puntos, tabla)
+
+    # Armo el polinomio
+    sumatoria = tabla[cantidad_puntos-1][1]
+    for i in range(1, cantidad_puntos):
+        sumatoria += (proterm_regresivo(i, x, puntos) * tabla[0][i])
 
     return sumatoria
 
@@ -33,13 +54,32 @@ def proterm(i, x, puntos):
     return pro
 
 
+def proterm_regresivo(i, x, puntos):
+    cantidad_puntos = len(puntos)
+    pro = 1
+    for j in range(1, i):
+        tmp = (x - puntos[cantidad_puntos-j][0])
+        pro = pro * tmp
+    return pro
+
+
 # Function for calculating
 # divided difference table
 def tabla_diferencias_divididas(puntos, tabla, cantidad_puntos):
     for i in range(1, cantidad_puntos):
         for j in range(cantidad_puntos - i):
             tabla[j][i] = ((tabla[j][i - 1] - tabla[j + 1][i - 1]) /
-                       (puntos[j][0] - puntos[i + j][0]))
+                           (puntos[j][0] - puntos[i + j][0]))
+    return tabla
+
+
+def tabla_diferencias_divididas_regresiva(puntos, tabla, cantidad_puntos):
+    for i in range(1, cantidad_puntos):
+        j = cantidad_puntos - 1
+        while j >= i:
+            tabla[j][i] = ((tabla[j][i - 1] - tabla[j - 1][i - 1]) /
+                           (puntos[j][0] - puntos[j - i][0]))
+            j = j-1
     return tabla
 
 
@@ -52,11 +92,15 @@ def print_tabla_diferencias_divididas(puntos, tabla):
         encabezado.append("Orden {}".format(i))
     resultado = Texttable()
     resultado.add_row(encabezado)
-    resultado.add_rows(tabla, header =False)
-    resultado.set_cols_align(list("c"*(cantidad_puntos+1)))
+    resultado.add_rows(tabla, header=False)
+    resultado.set_cols_align(list("c" * (cantidad_puntos + 1)))
     print(resultado.draw(), '\n')
 
 
-a = newton_gregory((1,1), (3,3), (4,13), (5,37), (7,151))
-print(a)
-print(simplify(a))
+# a = newton_gregory((1, 1), (3, 3), (4, 13), (5, 37), (7, 151))
+# print(a)
+# print(simplify(a))
+
+b = newton_gregory_regresivo((1, 1), (3, 3), (4, 13), (5, 37), (7, 151))
+print(b)
+print(simplify(b))
