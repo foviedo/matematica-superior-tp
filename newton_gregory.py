d87f4c7f898ae5f563af3eca79b192044a69be06
Fran <franciscodumont1@gmail.com>
2019-11-14 19:29:34 -0300

    Mostrar pasos en NG progresivo

--- newton_gregory.py
+++ newton_gregory.py
@@ -7,6 +7,10 @@ from texttable import Texttable
 def newton_gregory(*puntos: Tuple[int, int]):
     cantidad_puntos = len(puntos)
 
+    pasos = "Primero me fijo cuandos puntos tengo, en este caso son {}:\n\t{}\n".format(cantidad_puntos, puntos)
+    pasos += "\nPara realizar el método de Newton Gregory vamos a trabajar con la tabla de diferencias divididas progresivas.\n" \
+             "Con los puntos que tenemos la tabla nos queda así:.\n\n"
+
     # Creo la tabla llena de 0s
     tabla = [[0 for i in range(cantidad_puntos)] for j in range(cantidad_puntos)]
     # Cargo los f(x) de la tabla
@@ -15,16 +19,29 @@ def newton_gregory(*puntos: Tuple[int, int]):
     # Genero la tabla de diferencias divididas
     tabla = tabla_diferencias_divididas(puntos, tabla, cantidad_puntos);
 
-    print_tabla_diferencias_divididas(puntos, tabla)
+    pasos += tabla_diferencias_divididas_to_string(puntos, tabla)
+    pasos += "\n\nAhora, planteamos el polinomio:\n" \
+             "\tP(x)=a0+a1(x-x0)+a2(x-x0)(x-x1)+....+an(x-x0)(x-x1)....(x-xn-1)\n"
+    pasos += "Donde cada \"an\" es un elemento de la primera fila en nuestra tabla de diferencias divididas progresivas.\n\n"
+
 
     # Armo el polinomio
     sumatoria = tabla[0][1]
+    pasos += "Termino 1 de {}:\n".format(cantidad_puntos)
+    pasos += "\ta0 = {}\n".format(sumatoria)
+
     for i in range(2, cantidad_puntos+1):
         a_n = tabla[0][i]
         p = proterm(i-1, x, puntos)
+        pasos += "Termino {} de {}:\n".format(i, cantidad_puntos)
+        pasos += "\ta{} = {}\n\t{} * {}\n".format(i-1, a_n, a_n, p)
+
+
         sumatoria += ( a_n * p )
 
-    return simplify(sumatoria)
+    pasos +="\nSumando los terminos anteriores nuestro polinomio resulta:\n\tP(x) = {}\n\n".format(sumatoria)
+    pasos += "Y operando finalmente obtenemos:\n\tP(x) = {}\n".format(simplify(sumatoria))
+    return simplify(sumatoria), pasos
 
 
 def newton_gregory_regresivo(*puntos: Tuple[int, int]):
@@ -84,7 +101,7 @@ def tabla_diferencias_divididas_regresiva(puntos, tabla, cantidad_puntos):
     return tabla
 
 
-def print_tabla_diferencias_divididas(puntos, tabla):
+def tabla_diferencias_divididas_to_string(puntos, tabla):
     cantidad_puntos = len(tabla)
     for i, element in enumerate(tabla): # Con esto agrego la columna de x
         element.insert(0, puntos[i][0])
@@ -95,11 +112,12 @@ def print_tabla_diferencias_divididas(puntos, tabla):
     resultado.add_row(encabezado)
     resultado.add_rows(tabla, header=False)
     resultado.set_cols_align(list("c" * (cantidad_puntos + 1)))
-    print(resultado.draw(), '\n')
+    return resultado.draw()
 
 
-# a = newton_gregory((1, 1), (3, 3), (4, 13), (5, 37), (7, 151))
+# a, mensaje = newton_gregory((1, 1), (3, 3), (4, 13), (5, 37), (7, 151))
 # print(a)
+# print(mensaje)
 # print(simplify(a))
 #
 # b = newton_gregory_regresivo((1, 1), (3, 3), (4, 13), (5, 37), (7, 151))
